8c0f657e2053dab417a81eed15dcf69595f399a9
pau.lopez.78 <pau.lopez.78@gmail.com>
2018-05-14 11:26:57 +0200

    first commit

---
+++ VotingApp.Domain/Voting.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VotingApp.Domain
+{
+    public class Voting
+    {
+        public Voting()
+        {
+        }
+
+        public Dictionary<string, int> Votes { get; private set; }
+        public string Winner { get; private set;}
+
+        public void Start(params string[] options)
+        {
+            Votes = options.ToDictionary(x => x, _ => 0);
+        }
+
+        public void Vote(string option) => Votes[option]++;
+
+        public void Finish() 
+        {
+            var maxVotes = Votes.Max(x => x.Value);
+            Winner = Votes.First(x => x.Value == maxVotes).Key;
+        }
+    }
+
+}
