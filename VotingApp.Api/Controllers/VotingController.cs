ee0ab4e1b74c9f8839fb1e336f699b8a631d8959
pau.lopez.78 <pau.lopez.78@gmail.com>
2018-05-14 12:02:39 +0200

    votingapp api

---
+++ VotingApp.Api/Controllers/VotingController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VotingApp.Domain;
+
+namespace VotingApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class VotingController : Controller
+    {
+        private readonly Voting _voting;
+        public VotingController(Voting voting)
+        {
+            _voting = voting;
+        }
+
+        [HttpGet]
+        public object Get() => _voting.GetState();
+
+        [HttpPost]
+        public object Post([FromBody]string value)
+        {
+            _voting.Vote(value);
+            return _voting.GetState();
+        }
+
+        [HttpPut]
+        public object Put([FromBody]string value)
+        {
+        }
+
+        [HttpDelete]
+        public object Delete()
+        {
+        }
+    }
+}
