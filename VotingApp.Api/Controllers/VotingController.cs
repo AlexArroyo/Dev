a67888fa130a840bf2b955ca94dd7e43db4c2f5f
pau.lopez.78 <pau.lopez.78@gmail.com>
2018-05-14 12:35:12 +0200

    config example

--- VotingApp.Api/Controllers/VotingController.cs
+++ VotingApp.Api/Controllers/VotingController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using VotingApp.Domain;
 
 namespace VotingApp.Api.Controllers
@@ -10,10 +12,16 @@ namespace VotingApp.Api.Controllers
     [Route("api/[controller]")]
     public class VotingController : Controller
     {
+        private readonly int votingstep;
         private readonly Voting _voting;
-        public VotingController(Voting voting)
+        private readonly ILogger<VotingController> _logger;
+
+        public VotingController(Voting voting, ILogger<VotingController> logger, IConfiguration config)
         {
+            votingstep = config.GetValue<int>("VOTING_STEP", 1);
+            logger.LogInformation($"Voting step {votingstep}");
             _voting = voting;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -22,6 +30,7 @@ namespace VotingApp.Api.Controllers
         [HttpPost]
         public object Post([FromBody]string[] values)
         {
+            _logger.LogInformation("Starting Voting");
             _voting.Start(values);
             return _voting.GetState();
         }
